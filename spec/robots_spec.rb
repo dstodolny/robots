9ed74668420e3beeb2885fb91e35726e08a566e5
Dominik Stodolny <dom@dmk.st>
2016-11-14 22:13:53 +0000

    remove duplicate integration test

--- spec/robots_spec.rb
+++ spec/robots_spec.rb
@@ -4,11 +4,4 @@ describe Robots do
   it "has a version number" do
     expect(Robots::VERSION).not_to be nil
   end
-
-  it "consumes instructions and generates correct output" do
-    instructions = IO.read(File.dirname(__FILE__) + "/fixtures/example")
-    output = IO.read(File.dirname(__FILE__) + "/fixtures/example.out")
-
-    expect(Robots::Runner.new(instructions: instructions).execute).to eq(output)
-  end
 end
