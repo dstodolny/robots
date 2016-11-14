5aa98749da5e7c3d43eef2969271bbc50c916dc0
Dominik Stodolny <dom@dmk.st>
2016-11-14 20:12:17 +0000

    finish up robot class

--- lib/robots/robot.rb
+++ lib/robots/robot.rb
@@ -1,10 +1,25 @@
 module Robots
   class Robot
-    attr_reader :position, :orientation
+    attr_accessor :orientation, :position
+    attr_reader :commands, :status
 
-    def initialize(position:, orientation:)
+    def initialize(position:, orientation:, commands:, status: :operating)
       @position = position
       @orientation = orientation
+      @commands = commands
+      @status = status
+    end
+
+    def lose!
+      @status = :lost
+    end
+
+    def lost?
+      status == :lost
+    end
+
+    def set_position(new_position)
+      @position = new_position
     end
   end
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -3,13 +3,56 @@ require "spec_helper"
 describe Robots::Robot do
   describe "#initialize" do
     it "creates new robot with specified position and orientation" do
-      position = double
-      orientation = ::Robots::COMPAS.values.sample
+      robot = build_robot
 
-      robot = described_class.new(position: position, orientation: orientation)
-
-      expect(robot.position).to eq(position)
-      expect(robot.orientation).to eq(orientation)
+      expect(robot.position).to eq(Vector[1, 1])
+      expect(robot.orientation).to eq(:north)
+      expect(robot.commands).to eq("FRFR")
+      expect(robot.status).to eq(:operating)
     end
+  end
+
+  describe "#lose!" do
+    it "sets robot's status to lost" do
+      robot = build_robot
+
+      robot.lose!
+
+      expect(robot.status).to eq(:lost)
+    end
+  end
+
+  describe "#lost" do
+    it "returns true if robot is lost" do
+      robot = build_robot(status: :lost)
+
+      expect(robot.lost?).to eq(true)
+    end
+
+    it "returns false if robot is not lost" do
+      robot = build_robot
+
+      expect(robot.lost?).to eq(false)
+    end
+  end
+
+  describe "#set_position" do
+    it "sets the new position to the robot" do
+      robot = build_robot
+      new_position = Vector[2, 2]
+
+      robot.set_position(new_position)
+
+      expect(robot.position).to eq(new_position)
+    end
+  end
+
+  def build_robot(status: :operating)
+    described_class.new(
+      position: Vector[1, 1],
+      orientation: :north,
+      commands: "FRFR",
+      status: status
+    )
   end
 end
